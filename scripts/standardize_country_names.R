678148c08581aa1dbbab81a672a0f33286540510
Ellis Coldren <ecoldren@bu.edu>
2024-05-07 18:18:23 -0400

    hopefully removed some dependency

--- scripts/standardize_country_names.R
+++ scripts/standardize_country_names.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 standardize_country_names <- function(countries) {
   countries <- countries %>%
     tolower() %>%
@@ -35,41 +34,4 @@ standardize_country_names <- function(countries) {
     str_replace_all("china, people's republic of", "china") %>%
     str_replace_all("cote d'ivoire", "côte d'ivoire" )
   return(countries)
-
-=======
-standardize_country_names <- function(countries) {
-  countries %>%
-    tolower() %>%
-    str_replace_all("china - mainland", "china") %>%
-    str_replace_all("congo, democratic republic of the", "democratic replublic of congo") %>%
-    str_replace_all("congo, republic of the", "republic of congo") %>%
-    str_replace_all("congo (brazzaville)", "republic of congo") %>%
-    str_replace_all("congo (kinshasa)", "democratic republic of congo") %>%
-    str_replace_all("hong kong s.a.r.", "hong kong") %>%
-    str_replace_all("cape verde", "cabo verde") %>%
-    str_replace_all("hong kong sar", "hong kong") %>%
-    str_replace_all("islamic republic of iran", "iran") %>%
-    str_replace_all("kyrgyz republic", "kyrgyzstan") %>%
-    str_replace_all("lao p.d.r.", "laos") %>%
-    str_replace_all("macao sar", "macau") %>%
-    str_replace_all("north macedonia", "macedonia") %>%
-    str_replace_all("russian federation", "russia") %>%
-    str_replace_all("korea, republic of", "south korea") %>%
-    str_replace_all("taiwan province of china", "taiwan") %>%
-    str_replace_all("united republic of tanzania", "tanzania") %>%
-    str_replace_all("türkiye, republic of", "turkey") %>%
-    str_replace_all("united kingdom", "great britain") %>%
-    str_replace_all("bolivarian republic of venezuela", "venezuela") %>%
-    str_replace_all("congo, dem. rep. of the", "democratic republic of congo") %>%
-    str_replace_all("slovak republic", "slovakia") %>%
-    str_replace_all("st lucia", "saint lucia") %>%
-    str_replace_all("st vincent", "saint vincent and the grenadines") %>%
-    str_replace_all("sri lanka", "sri lanka") %>%
-    str_replace_all("bahamas, the", "bahamas") %>%
-    str_replace_all("gambia, the", "gambia") %>%
-    str_replace_all("myanmar \\(myanmar\\)", "myanmar") %>%
-    str_replace_all("united states of america", "united states") %>%
-    str_replace_all("china, people's republic of", "china") %>%
-    str_replace_all("cote d'ivoire", "côte d'ivoire" )
->>>>>>> d72f92ac802be305ceb59b58f88a1fc5deae3d69
 }
